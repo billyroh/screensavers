2e3139593983f8d329b9e3fdd5bd6e5d398a6ea1
Billy Roh <billyroh@users.noreply.github.com>
2019-09-27 17:02:01 -0700

    Add matrix options (#14)
    * wip; seems a bit buggy
    
    * add corpus options
    
    * revise colour palette, add some more matrix options

--- matrix/scripts.js
+++ matrix/scripts.js
@@ -1,10 +1,12 @@
 const svg = document.querySelector('svg');
-let corpus;
+const animationLength = 5000;
+const animationDelay = 100;
+let corpus, palette;
+let columnIsAnimating = [];
 
 // 1. Create columns and rows to fill the viewport
 // 2. Fill each of the cells with a character
 // 3. Iterate through each of the columns and set some speed of rain
-// 4. 
 
 function main() {
   initialize();
@@ -19,11 +21,14 @@ function initialize() {
   let textHeight = 18;
   let numberOfColumns = Math.ceil(viewPortWidth / textWidth) + 4;
   let numberOfRows = Math.ceil(viewPortHeight / textHeight) + 4;
-  
-  let numericalCorpus = '12345678990';
-  let japaneseCorpus = '道可非常可名ジスセソタダチヂツテトナニヌネノハバパヒフヘホマミムメモ';
-  let symbolCorpus = '☺✑♡☂⋮✐☼☏⇄✏✎№Ω℞※§∜☽♀♂⚢₹☁↷☻†¤¢℅☎↻❤♨⟷¶☙µ≤'
-  corpus = numericalCorpus + japaneseCorpus;
+
+  corpus = getCorpus('japanese');
+  palette = getPalette('classic');
+  // palette = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'purple'];
+
+  for (let i = 0; i < numberOfColumns; i++) {
+    columnIsAnimating.push(false);
+  }
   
   for (let c = 0; c < numberOfColumns; c++) {
     let column = document.createElementNS('http://www.w3.org/2000/svg', 'g');
@@ -37,6 +42,39 @@ function initialize() {
       text.innerHTML = _.sample(corpus);
       column.append(text);
     }
+  }
+}
+
+function getCorpus(corpusType) {
+  let numericalCorpus = '12345678990';
+  let corpus = numericalCorpus;
+
+  if (corpusType === 'japanese') {
+    corpus += '道可非常可ジスセソタチツテトナニヌネノハヒフヘホマミムメ';
+  } else if (corpusType === 'korean') {
+    corpus += 'ㅁㄴㅇㄹㅎㅗㅓㅏㅣㅂㅈㄷㄱㅅㅆㅃㅉㄸㄲㅆㅋㅌㅊㅍㅐㅔㅠㅜㅡ'
+  } else if (corpusType === 'arabic') {
+    corpus += 'وجّهالممثلورمحمدعليرسالةإلىالرئيسعبدالفتاح';
+  } else if (corpusType === 'hebrew') {
+    corpus += 'מתרחשסביבנולצנוחלגובהכזהזובהחלטחוויה'
+  } else if (corpusType === 'wingdings') {
+    corpus += '☺✑♡☂⋮✐☼☏⇄✏✎№Ω℞※§∜☽♀♂⚢₹☁↷☻†¤¢℅☎↻❤♨⟷¶☙µ≤';
+  } else {
+    corpus += '道可非常可ジスセソタチツテトナニヌネノハヒフヘホマミムメ'; 
+  }
+
+  return corpus;
+}
+
+function getPalette(paletteType) {
+  if (paletteType === 'classic') {
+    return ['green'];
+  } else if (paletteType === 'rainbow') {
+    return ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'purple'];
+  } else if (paletteType === 'pastel') {
+    return ['pastel1', 'pastel2', 'pastel3', 'pastel4'];
+  } else {
+    return ['green'];
   }
 }
 
@@ -56,23 +94,49 @@ function randomize() {
 function animate() {
   let columns = svg.childNodes;
   setInterval(() => {
-    for (const column of columns) {
-      if (_.random(100) < 1) {
-        let i = 1;
-        for (const text of column.childNodes) {
-          setTimeout(() => {
-            text.setAttribute('class', 'transition');
-          }, i * 100);
-
-          setTimeout(() => {
-            text.setAttribute('class', '');
-          }, (i * 100) + 5000);
-
-          i++;
-        }
+    for (let columnIndex = 0; columnIndex < columns.length; columnIndex++) {
+      if (_.random(100) < 1 && !columnIsAnimating[columnIndex]) {
+        animateColumn(columns, columnIndex);
       }
     }
   }, 250);
 }
+
+async function animateColumn(columns, columnIndex) {
+  let color = _.sample(palette);
+  let column = columns[columnIndex];
+  let numberOfRows = column.childNodes.length;
+  columnIsAnimating[columnIndex] = true;
+
+  for (let rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
+    let text = column.childNodes[rowIndex];
+    await animateCell(text, color);
+  }
+
+  columnIsAnimating[columnIndex] = false;
+}
+
+async function animateCell(text, color) {
+  text.setAttribute('class', color);
+
+  setTimeout(() => {
+    text.removeAttribute('class');
+  }, animationLength + animationDelay)
+
+  return new Promise(resolve => {
+    setTimeout(resolve, animationDelay);
+  });
+}
+
+const paletteSelector = document.querySelector('select.palette-selector');
+const corpusSelector = document.querySelector('select.corpus-selector');
+
+paletteSelector.addEventListener('change', (event) => {
+  palette = getPalette(event.target.value);
+});
+
+corpusSelector.addEventListener('change', (event) => {
+  corpus = getCorpus(event.target.value);
+});
 
 main();
